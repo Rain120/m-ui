ad2e2ca69001df7a441e1796368db0b8bf002672
Rain120 <chao1202016@outlook.com>
2019-08-05 20:52:06 +0800

    feat: alert component

--- components/Alert/index.tsx
+++ components/Alert/index.tsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import className from 'classnames';
 import { setPrefix } from '../_util/setPrefix';
-import './style/index';
+import './style';
 
 interface IAlertProps {
-  type?: 'success' | 'error' | 'warn' | 'info'
-  message: React.ReactNode
-  className?: String
+  type?: 'success' | 'error' | 'warn' | 'info';
+  message: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 export default class Alert extends Component<IAlertProps> {
 
   render() {
-    const { type = 'info', message } = this.props;
+    const { type = 'info', message, style } = this.props;
     const prefix = setPrefix('alert');
 
     return (
       <div
-        className={className(prefix, `${prefix}-${type}`)}>
+        className={className(prefix, `${prefix}-${type}`)}
+        style={style}
+      >
           {message}
       </div>
     )
